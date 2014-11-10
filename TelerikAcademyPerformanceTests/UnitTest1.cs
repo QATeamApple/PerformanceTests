0a5dfeba0ddab8ea718a5e5812890d453c105baa
ms-nenova <ms.nenova@gmail.com>
2014-11-10 10:28:21 +0200

    Initial unit test project

---
+++ TelerikAcademyPerformanceTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace TelerikAcademyPerformanceTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
